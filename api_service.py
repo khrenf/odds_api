6a38293e05a8332a6c0760dce3829ccdb72b5eee
khrenf <kobyrenfert2025@u.northwestern.edu>
2024-07-21 13:27:36 -0500

    Testing update with insecure response

--- api_service.py
+++ api_service.py
@@ -29,5 +29,5 @@ def get_no_vig(param1: str, param2: str):
     return calculate(odds_one, odds_two)
     
 if __name__ == '__main__':
-    # uvicorn.run(app, host='0.0.0.0', port=8000)
-    uvicorn.run(app, host="0.0.0.0", port=8000, ssl_keyfile="/etc/letsencrypt/live/kobyrenfert.me/privkey.pem", ssl_certfile="/etc/letsencrypt/live/kobyrenfert.me/fullchain.pem")
+    uvicorn.run(app, host='0.0.0.0', port=8000)
+    # uvicorn.run(app, host="0.0.0.0", port=8000, ssl_keyfile="/etc/letsencrypt/live/kobyrenfert.me/privkey.pem", ssl_certfile="/etc/letsencrypt/live/kobyrenfert.me/fullchain.pem")
